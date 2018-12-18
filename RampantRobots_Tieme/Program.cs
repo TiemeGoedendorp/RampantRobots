f52bb96fe40d957efd23090a31e4e4020cb01018
Tieme Goedendorp <299001@student.fontys.nl>
2018-12-18 13:08:16 +0100

    Add project files.

---
+++ RampantRobots_Tieme/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RampantRobots_Tieme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellllcoommeee toooo rraamppaannnttt rooobbboootttssss");
+
+            Console.WriteLine("Press enter to quit");
+            Console.ReadLine();
+
+        }
+    }
+}
